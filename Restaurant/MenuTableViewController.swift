206753b65b4ef177c21f5a753874d603d0d38782
maahd <maahdsh@gmail.com>
2019-09-06 23:50:16 +0200

    Menu Items

--- Restaurant/MenuTableViewController.swift
+++ Restaurant/MenuTableViewController.swift
@@ -9,5 +9,48 @@
 import UIKit
 
 class MenuTableViewController: UITableViewController {
+    var menuItems = [MenuItem]()
     var category: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = category.capitalized
+        MenuController.shared.fetchMenuItems(forCategory: category) { (menuItems) in
+            if let menuItems = menuItems {
+                self.updateUI(with: menuItems)
+            }
+        }
+    }
+    
+    func updateUI(with menuItems: [MenuItem]) {
+        DispatchQueue.main.async {
+            self.menuItems = menuItems
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCellIdentifier", for: indexPath)
+        configure(cell, forItemAt: indexPath)
+        return cell
+    }
+    
+    func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let menuItem = menuItems[indexPath.row]
+        cell.textLabel?.text = menuItem.name
+        cell.detailTextLabel?.text = String(format: "$%.2f", menuItem.price)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MenuDetailSegue" {
+            let menuItemDetailViewController = segue.destination as! MenuItemDetailViewController
+            let index = tableView.indexPathForSelectedRow!.row
+            menuItemDetailViewController.menuItem = menuItems[index]
+        }
+    }
 }
